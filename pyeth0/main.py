2ee7965c75b2544a4a1b2d5bdba21d3a2d9c8237
Daniel Lima <dnlima77237@gmail.com>
2024-10-22 15:54:36 -0300

    Upd

--- pyeth0/main.py
+++ pyeth0/main.py
@@ -7,7 +7,7 @@ from pyeth0.port_scanner import PortScanner
 
 host_name = "google.com"
 host_ip = ip_address(socket.gethostbyname(host_name))
-ports = PortScanner.scan_specific_ports(host_ip, [80, 443, 22, 20])
+ports = PortScanner.get_open_ports(host_ip, (20, 444))
 
 print(f"Open ports for {host_name} ({host_ip})")
 for port in ports:
