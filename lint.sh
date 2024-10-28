9fd4ce34f2d896207cfcf500ffae624c740f124e
Daniel Lima <dnlima77237@gmail.com>
2024-10-28 12:50:45 -0300

    Fixed scripts

--- lint.sh
+++ lint.sh
@@ -3,4 +3,5 @@
 poetry run black pyeth0
 poetry run isort pyeth0
 poetry run pylint pyeth0 >> pylint.log
+echo "##########################################" >> mypy.log
 poetry run mypy pyeth0 >> mypy.log
